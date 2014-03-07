c5bfa4724615ece4d31edfe378cdf2e8d6a67d00
jinjun001 <jinjun001@gmail.com>
2014-03-07 21:53:33 +0800

    add cardbox.js

---
+++ js/CardBox.js
@@ -0,0 +1,13 @@
+﻿var app = angular.module('app', ['ngResource']);
+app.factory('Cardbox', ['$resource', function ($resource) {
+    return $resource('/data/carbox.json');
+}]);
+app.controller('CardBoxCtrl', ['$scope', 'Cardbox',
+function ($scope, Cardbox) {
+    var boxes = Cardbox.query(function () {
+        $scope.Cardboxes = boxes;
+    });
+    $scope.OpenMoreBox = function (boxId) {
+        alert(boxId);
+    }
+}]);
